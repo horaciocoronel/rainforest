543147f563d2e2fd39c4b2efa72c0885e82f0a6f
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-18 12:02:08 -0400

    adds validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+
+
+    validates :name,            presence: true
+    validates :description,     presence: true
+    validates :price_in_cents, numericality: { only_integer: true }
+
+
 end
