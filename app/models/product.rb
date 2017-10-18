9e2994efd8c0f99658f65b9bbed4ebe6d75b34d9
horaciocoronel <arielc@gmail.com>
2017-10-18 13:49:09 -0400

    Add Dollar conversion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
 
+    def price_in_dollars
+      price_in_dollars = price_in_cents.to_f / 100
+      sprintf("$%.2f", price_in_dollars)
+    end
+
 
     validates :name, :description, :price_in_cents, presence: { message: "Please fill out %{attribute}" }
     validates :price_in_cents, numericality: { message: "Please enter a integer"}
