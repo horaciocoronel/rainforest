e8034d72171c729742fdb9c0f6a4f9771c29cbc8
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-18 11:37:14 -0400

    adds at 11:37

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+
   end
 
   def create
@@ -27,7 +28,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-
+    @product = Product.find(params[:id])
   end
 
   def update
