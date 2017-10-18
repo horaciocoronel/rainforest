8216ad247034140cbb0e66c1610f6dec93922705
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-18 10:55:59 -0400

    adds forms view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,14 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    
   end
 
   def create
-
+    @product = Product.new
   end
 
   def new
-
+    @product = Product.new
   end
 
   def edit
