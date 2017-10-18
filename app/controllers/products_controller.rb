d845d629dae275d4e9421e544d0421da5cb1043d
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-18 11:25:29 -0400

    Adds add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,22 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def create
+  def new
     @product = Product.new
   end
 
-  def new
+  def create
     @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to "/products"
+    else
+      redirect_to "/products/new"
+    end
   end
 
   def edit
@@ -28,5 +38,9 @@ class ProductsController < ApplicationController
 
   end
 
+
+  def product_params
+    params.required(:product).permit(:name, :description, :price_in_cents)
+  end
 
 end
