17365f5972e1f460f43462c0741c86682cea2aa3
horaciocoronel <arielc@gmail.com>
2017-10-19 13:44:12 -0400

    Create sessions controller, remove unnecessary views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
 
 
       resource  :users, only: %i(create new)
+      resource :sessions, only %i(create new destroy)
       resources :products do
       resources :reviews, except: [:index, :new, :show]
     end
