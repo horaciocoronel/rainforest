c24182cca87a98056d384b2e5e1030dba83d2d0c
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-19 13:26:42 -0400

    remove create and view and add restful routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/create'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
 
+
+      resource  :users, only: %i(create new)
       resources :products do
       resources :reviews, except: [:index, :new, :show]
     end
